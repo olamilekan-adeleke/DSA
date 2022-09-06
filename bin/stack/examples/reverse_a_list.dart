f50eef8a92b8091c4d1c36989c7d1ee496a4ab29
olamilekan-adeleke <olamilekanadeleke123@gmail.com>
2022-09-06 22:40:57 +0100

    added reverse_a_list

---
+++ bin/stack/examples/reverse_a_list.dart
@@ -0,0 +1,22 @@
+import '../implementation/stack.dart';
+
+/// Example: Reverse a List
+///
+/// Create a function that prints the contents of a list in reverse order.
+void printListInReverse<E>(List<E> list) {
+  final Stack<E> stack = Stack<E>();
+
+  for (E val in list) {
+    stack.push(val);
+  }
+
+  while (stack.isEmpty != true) {
+    print(stack.pop());
+  }
+}
+
+void main() {
+  final List<int> list = <int>[0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  printListInReverse(list);
+}
